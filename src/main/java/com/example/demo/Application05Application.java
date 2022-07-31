ea5facc964d0f3030d94dfcfa8d8f84097d2a342
aibooks14 <101002119+aibooks14@users.noreply.github.com>
2022-07-31 16:49:49 +0900

    Add files via upload

---
+++ src/main/java/com/example/demo/Application05Application.java
@@ -0,0 +1,38 @@
+package com.example.demo;
+
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+//import com.example.demo.entity.Student;
+//import com.example.demo.repository.StudentRepository;
+
+@SpringBootApplication
+public class Application05Application  {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application05Application.class, args);
+	}
+	
+//	@Autowired
+//	private StudentRepository studentRepository ;
+	
+	
+//	@Override
+//	public void run(String... args) throws Exception {
+//		Student student1 = new Student("Ramesh", "Kumar", "ramesh@gmail.com") ;
+//		studentRepository.save(student1) ;
+//		
+//		Student student2 = new Student("Amit", "Kumar", "amit@test.com") ;
+//		studentRepository.save(student2) ;
+//		
+//		Student student3 = new Student("Mohit", "Sharma", "m-sharma@test.com") ;
+//		studentRepository.save(student3) ;
+//		
+//		Student student4 = new Student("Amar", "Kumar", "amar@test.com") ;
+//		studentRepository.save(student4) ;
+		
+//	}
+
+}
